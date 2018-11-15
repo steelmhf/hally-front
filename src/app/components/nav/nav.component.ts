4885f3bd98e47c4e334abf3bb09cd2c4fb56762b
Cristhyan De Marchena <demarchenac@uninorte.edu>
2018-11-14 23:39:45 -0500

    Revert "yes"
    This reverts commit bec2812324914fa94daecb05fd70b5c336be4ba3.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import {UserService} from './../../services/user.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,15 +7,7 @@ import {UserService} from './../../services/user.service';
 })
 export class NavComponent implements OnInit {
 
-  onLogOutClick(){
-    /* aqui va la logica del logout */
-    this.router.navigate(['/']);
-  }
-
-  constructor(
-    private userService: UserService,
-    private router: Router 
-  ) { }
+  constructor() { }
 
   ngOnInit() {
   }
