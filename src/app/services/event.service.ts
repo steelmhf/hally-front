ae427163ef33610fd4fd0e2c3a36d89ba88ab2bf
Steel Herrera <sherrera@formax.cl>
2018-11-14 23:33:18 -0500

    Added events services

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -5,21 +5,54 @@ import { Injectable } from '@angular/core';
 })
 export class EventService {
 
-  constructor() { }
+  constructor() { 
+    localStorage.setItem("events", JSON.stringify([
+      {
+      "id": "QA2DJF12S",
+      "name": "Halloween 31 de octubre",
+      "date": "2018-11-11T01:08:43.511Z",
+      "status": 1,
+      "paymentInfo": {
+        "somePaymentInfoField": "123"
+      },
+      "spaceId": "Z43KZJS5L",
+      "owner": {
+        "id": "1143166573",
+        "name": "james",
+        "contact": {
+          "email": "jfritz@hallycompany.co",
+          "phone": "573015648789",
+          "address": "Cra 49#100-15"
+        }
+      },
+      "guestIds": [
+        "1145167984"
+      ]
+      }
+    ]));
+  }
 
   getAll(){
-    return [];
+    return JSON.parse(localStorage.getItem("events"));
   }
   
   getById(id){
-    return [];
+    return JSON.parse(localStorage.getItem("events")).find(x => x.id === id);
   }
 
-  getByUserId(id){
-    return [];
+  getByOwnerId(id){
+    return JSON.parse(localStorage.getItem("events")).find(x => x.owner.id === id);
+  }
+
+  getByGuestId(id){
+    return JSON.parse(localStorage.getItem("events")).find((x) => {
+      return (x.guestIds.find(y => y === id)) ? true : false;
+    });
   }
 
   insertEvent(event){
-    
+    const events = JSON.parse(localStorage.getItem("events"));
+    events.push(event);
+    localStorage.setItem("events", JSON.stringify(events));
   }
 }
