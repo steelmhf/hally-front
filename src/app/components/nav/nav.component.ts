bec2812324914fa94daecb05fd70b5c336be4ba3
Cristhyan De Marchena <demarchenac@uninorte.edu>
2018-11-14 23:39:09 -0500

    yes

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {UserService} from './../../services/user.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  onLogOutClick(){
+    /* aqui va la logica del logout */
+    this.router.navigate(['/']);
+  }
+
+  constructor(
+    private userService: UserService,
+    private router: Router 
+  ) { }
 
   ngOnInit() {
   }
